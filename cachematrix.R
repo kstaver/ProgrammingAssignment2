611e9c3cd48382cb205fc94e2225d6876d3e9304
Kendra Staver <staverkendra@gmail.com>
2021-03-07 22:27:39 -0800

    Edited makeCacheMatrix and cacheSolve
    This is my completed version of the Lexical Scoping assignment for Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Our Week 3 assignment was to design two functions, makeCacheMatrix and
+## cacheSolve, to better understand how to create and manipulate a matrix in R.
+## Author: Kendra Staver
+## Date: 3/7/2021
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a matrix of nrows and ncols. Define functions that: display the matrix,
+## set the inverse of the matrix, and display the inverse of the matrix.
+makeCacheMatrix <- function(x = matrix()){
+  invv <- NULL
+  set <- function(y){
+    x <<- y
+    invsample <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function(){inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## Calculate the inverse of the matrix made in makeCacheMatrix. Return the inverse
+## matrix.
+cacheSolve <- function(x, ...){
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  matrixData <- x$get()
+  inv <- solve(matrixData, ...)
+  x$setInverse(inv)
+  inv
 }
